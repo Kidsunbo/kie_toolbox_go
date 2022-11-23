127be89d968cf0d500b75f093585a1a42117e0f9
sunbo.kie <sunbo.kie@bytedance.com>
2022-11-23 15:49:12 +0800

    [ADD] add more test code

--- logs/logs_test.go
+++ logs/logs_test.go
@@ -3,6 +3,7 @@ package logs
 import (
 	"context"
 	"fmt"
+	"os"
 	"testing"
 )
 
@@ -47,6 +48,53 @@ func TestLogsWithContextLogID(t *testing.T) {
 		}
 		t.Fail()
 	}()
+	CtxDebug(ctx, "hello %v, and this is %v with age %v", "world", "tom", 12)
+	CtxInfo(ctx, "hello %v, and this is %v with age %v", "world", "tom", 12)
+	CtxWarn(ctx, "hello %v, and this is %v with age %v", "world", "tom", 12)
+	CtxError(ctx, "hello %v, and this is %v with age %v", "world", "tom", 12)
+	CtxFatal(ctx, "hello %v, and this is %v with age %v", "world", "tom", 12)
+}
+
+func TestLogsSetOutputFile(t *testing.T) {
+	ctx := context.Background()
+	ctx = context.WithValue(ctx, "LOG_ID", "1234567890")
+	defer func() {
+		err := os.Remove("hello.log")
+		if err != nil {
+			fmt.Println("[NOTICE] Fail to delete file", err)
+		}
+		if a := recover(); a != nil {
+			fmt.Println("[PASS]")
+			return
+		}
+		t.Fail()
+	}()
+	Default().SetOutputFile("hello.log")
+	CtxDebug(ctx, "hello %v, and this is %v with age %v", "world", "tom", 12)
+	CtxInfo(ctx, "hello %v, and this is %v with age %v", "world", "tom", 12)
+	CtxWarn(ctx, "hello %v, and this is %v with age %v", "world", "tom", 12)
+	CtxError(ctx, "hello %v, and this is %v with age %v", "world", "tom", 12)
+
+	Default().SetOutputFile("")
+	CtxDebug(ctx, "hello %v, and this is %v with age %v", "world", "tom", 12)
+	CtxInfo(ctx, "hello %v, and this is %v with age %v", "world", "tom", 12)
+	CtxWarn(ctx, "hello %v, and this is %v with age %v", "world", "tom", 12)
+	CtxError(ctx, "hello %v, and this is %v with age %v", "world", "tom", 12)
+	CtxFatal(ctx, "hello %v, and this is %v with age %v", "world", "tom", 12)
+}
+
+func TestLogsSetLevel(t *testing.T) {
+	ctx := context.Background()
+	ctx = context.WithValue(ctx, "LOG_ID", "1234567890")
+	defer func() {
+		if a := recover(); a != nil {
+			fmt.Println("[PASS]")
+			return
+		}
+		t.Fail()
+	}()
+	Default().SetLevel(LevelDebug)
+	Default().SetPathLength(3)
 	CtxDebug(ctx, "hello %v, and this is %v with age %v", "world", "tom", 12)
 	CtxInfo(ctx, "hello %v, and this is %v with age %v", "world", "tom", 12)
 	CtxWarn(ctx, "hello %v, and this is %v with age %v", "world", "tom", 12)
