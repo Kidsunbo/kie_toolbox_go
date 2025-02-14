f3352e4ceb198d04707e622ba4d3e3c2907f94f6
Kidsunbo <qiongxiaozi158@sina.com>
2025-02-14 11:37:47 +0000

    [ADD] finish kflow

--- kflow/kflow_test.go
+++ kflow/kflow_test.go
@@ -3,13 +3,16 @@ package kflow
 import (
 	"context"
 	"fmt"
+	"sync"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
 type State struct {
+	Lock           sync.Mutex
+	Stamps         []string
+	ConcurrentInfo []bool
 }
 
 type BasicNode struct {
@@ -20,9 +23,10 @@ func (b BasicNode) Name() string {
 	return b.name
 }
 
-func (b BasicNode) Run(ctx context.Context, state State) error {
-	time.Sleep(1 * time.Second)
-	fmt.Println(b.name)
+func (b BasicNode) Run(ctx context.Context, state *State) error {
+	state.Lock.Lock()
+	defer state.Lock.Unlock()
+	state.Stamps = append(state.Stamps, b.name)
 	return nil
 }
 
@@ -34,9 +38,11 @@ func (b FlowNode) Name() string {
 	return b.name
 }
 
-func (b FlowNode) Run(ctx context.Context, state State, plan *Plan) error {
-	time.Sleep(1 * time.Second)
-	fmt.Println(b.name, plan.InParallel())
+func (b FlowNode) Run(ctx context.Context, state *State, plan *Plan) error {
+	state.Lock.Lock()
+	defer state.Lock.Unlock()
+	state.Stamps = append(state.Stamps, b.name)
+	state.ConcurrentInfo = append(state.ConcurrentInfo, plan.InParallel())
 	return nil
 }
 
@@ -49,10 +55,10 @@ func (m StringDependence) Dependence() []string {
 }
 
 type ConditionDependence struct {
-	dependence []*Dependence[State]
+	dependence []*Dependence[*State]
 }
 
-func (m ConditionDependence) Dependence() []*Dependence[State] {
+func (m ConditionDependence) Dependence() []*Dependence[*State] {
 	return m.dependence
 }
 
@@ -77,7 +83,7 @@ type NodeType2 struct {
 	ConditionDependence
 }
 
-func NewNodeType2(name string, dep []*Dependence[State]) *NodeType2 {
+func NewNodeType2(name string, dep []*Dependence[*State]) *NodeType2 {
 	return &NodeType2{
 		BasicNode: BasicNode{
 			name: name,
@@ -109,7 +115,7 @@ type NodeType4 struct {
 	ConditionDependence
 }
 
-func NewNodeType4(name string, dep []*Dependence[State]) *NodeType4 {
+func NewNodeType4(name string, dep []*Dependence[*State]) *NodeType4 {
 	return &NodeType4{
 		FlowNode: FlowNode{
 			name: name,
@@ -120,24 +126,213 @@ func NewNodeType4(name string, dep []*Dependence[State]) *NodeType4 {
 	}
 }
 
-func TestAddNodes(t *testing.T) {
-	node := new(Node[State])
+func TestNormalNodeGraph1(t *testing.T) {
+	node := new(Node[*State])
 
-	eng := NewEngine[State]("")
+	eng := NewEngine[*State]("")
 	assert.NoError(t, AddNode(eng, NewNodeType3("Type1_1", nil)))
 	assert.NoError(t, AddNode(eng, NewNodeType3("Type1_2", []string{"Type1_1"})))
 	assert.NoError(t, AddNode(eng, NewNodeType3("Type1_3", []string{"Type1_1"})))
 	assert.NoError(t, AddNode(eng, NewNodeType3("Type1_4", []string{"Type1_1"})))
 
-	assert.NoError(t, AddNode(eng, NewNodeType4("Type2_1", []*Dependence[State]{
-		node.StaticDependence("Type1_3"), node.ConditionalDependence("Type1_1", func(ctx context.Context, s State) bool { return false }, []string{"Type1_2"}),
+	assert.NoError(t, AddNode(eng, NewNodeType4("Type2_1", []*Dependence[*State]{
+		node.StaticDependence("Type1_3"), node.ConditionalDependence("Type1_1", func(ctx context.Context, s *State) bool { return false }, []string{"Type1_2"}),
 	})))
-	assert.NoError(t, AddNode(eng, NewNodeType4("Type2_2", []*Dependence[State]{
-		node.StaticDependence("Type1_2"), node.ConditionalDependence("Type2_1", func(ctx context.Context, s State) bool { return true }, []string{"Type1_2", "Type1_4"}),
+	assert.NoError(t, AddNode(eng, NewNodeType4("Type2_2", []*Dependence[*State]{
+		node.StaticDependence("Type1_2"), node.ConditionalDependence("Type2_1", func(ctx context.Context, s *State) bool { return true }, []string{"Type1_2", "Type1_4"}),
 	})))
 
 	assert.NoError(t, eng.Prepare())
+	// fmt.Println(eng.Dot())
 
-	assert.NoError(t, eng.Run(context.Background(), State{}, "Type2_2"))
+	state := new(State)
+	assert.NoError(t, eng.Run(context.Background(), state, "Type1_1"))
+	assert.Equal(t, []string{"Type1_1"}, state.Stamps)
+	assert.Equal(t, []bool{false}, state.ConcurrentInfo)
+
+	state = new(State)
+	assert.NoError(t, eng.Run(context.Background(), state, "Type2_1"))
+	assert.Equal(t, "Type1_1", state.Stamps[0])
+	assert.ElementsMatch(t, []string{"Type1_2", "Type1_3"}, state.Stamps[1:3])
+	assert.Equal(t, "Type2_1", state.Stamps[3])
+	assert.Equal(t, []bool{false, true, true, false}, state.ConcurrentInfo)
+
+	state = new(State)
+	assert.NoError(t, eng.Run(context.Background(), state, "Type2_2"))
+	assert.Equal(t, "Type1_1", state.Stamps[0])
+	assert.ElementsMatch(t, []string{"Type1_2", "Type1_4"}, state.Stamps[1:3])
+	assert.Equal(t, "Type1_3", state.Stamps[3])
+	assert.Equal(t, "Type2_1", state.Stamps[4])
+	assert.Equal(t, "Type2_2", state.Stamps[5])
+	assert.Equal(t, []bool{false, true, true, false, false, false}, state.ConcurrentInfo)
+}
+
+func TestNormalNodeGraph2(t *testing.T) {
+	node := new(Node[*State])
+
+	eng := NewEngine[*State]("")
+	assert.NoError(t, AddNode(eng, NewNodeType3("Type1_1", nil)))
+	assert.NoError(t, AddNode(eng, NewNodeType3("Type1_2", nil)))
+	assert.NoError(t, AddNode(eng, NewNodeType3("Type1_3", nil)))
+	assert.NoError(t, AddNode(eng, NewNodeType3("Type1_4", []string{"Type1_1"})))
+	assert.NoError(t, AddNode(eng, NewNodeType3("Type1_5", []string{"Type1_6"})))
+	assert.NoError(t, AddNode(eng, NewNodeType3("Type1_6", nil)))
+
+	assert.NoError(t, AddNode(eng, NewNodeType4("Type2_1", []*Dependence[*State]{
+		node.StaticDependence("Type1_3"),
+		node.ConditionalDependence("Type1_5", func(ctx context.Context, s *State) bool { return false }, []string{"Type1_2"}),
+	})))
+	assert.NoError(t, AddNode(eng, NewNodeType4("Type2_2", []*Dependence[*State]{
+		node.ConditionalDependence("Type2_1", func(ctx context.Context, s *State) bool { return true }, []string{"Type1_2", "Type1_4"}),
+	})))
+
+	assert.NoError(t, eng.Prepare())
+	// fmt.Println(eng.Dot())
+
+	state := new(State)
+	assert.NoError(t, eng.Run(context.Background(), state, "Type1_1"))
+	assert.Equal(t, []string{"Type1_1"}, state.Stamps)
+	assert.Equal(t, []bool{false}, state.ConcurrentInfo)
+
+	state = new(State)
+	assert.NoError(t, eng.Run(context.Background(), state, "Type2_1"))
+	assert.ElementsMatch(t, []string{"Type1_2", "Type1_3"}, state.Stamps[0:2])
+	assert.Equal(t, "Type2_1", state.Stamps[2])
+	assert.Equal(t, []bool{true, true, false}, state.ConcurrentInfo)
+
+	state = new(State)
+	assert.NoError(t, eng.Run(context.Background(), state, "Type2_2"))
+	assert.ElementsMatch(t, []string{"Type1_1", "Type1_2", "Type1_4"}, state.Stamps[0:3])
+	assert.Equal(t, "Type1_3", state.Stamps[3])
+	assert.Equal(t, "Type2_1", state.Stamps[4])
+	assert.Equal(t, "Type2_2", state.Stamps[5])
+	assert.Equal(t, []bool{true, true}, state.ConcurrentInfo[:2])
+	assert.Equal(t, []bool{false, false, false}, state.ConcurrentInfo[3:])
+}
+
+func TestNormalNodeGraph3(t *testing.T) {
+	node := new(Node[*State])
+
+	eng := NewEngine[*State]("")
+	assert.NoError(t, AddNode(eng, NewNodeType3("Type1_1", nil)))
+	assert.NoError(t, AddNode(eng, NewNodeType3("Type1_2", nil)))
+	assert.NoError(t, AddNode(eng, NewNodeType3("Type1_3", nil)))
+	assert.NoError(t, AddNode(eng, NewNodeType3("Type1_4", []string{"Type1_1"})))
+	assert.NoError(t, AddNode(eng, NewNodeType3("Type1_5", []string{"Type1_6"})))
+	assert.NoError(t, AddNode(eng, NewNodeType3("Type1_6", nil)))
+
+	assert.NoError(t, AddNode(eng, NewNodeType4("Type2_1", []*Dependence[*State]{
+		node.StaticDependence("Type1_3"),
+		node.ConditionalDependence("Type1_5", func(ctx context.Context, s *State) bool { return false }, []string{"Type1_2"}),
+	})))
+	assert.NoError(t, AddNode(eng, NewNodeType4("Type2_2", []*Dependence[*State]{
+		node.ConditionalDependence("Type2_1", func(ctx context.Context, s *State) bool { return false }, []string{"Type1_2", "Type1_4"}),
+	})))
+
+	assert.NoError(t, eng.Prepare())
+	// fmt.Println(eng.Dot())
+
+	state := new(State)
+	assert.NoError(t, eng.Run(context.Background(), state, "Type1_1"))
+	assert.Equal(t, []string{"Type1_1"}, state.Stamps)
+	assert.Equal(t, []bool{false}, state.ConcurrentInfo)
+
+	state = new(State)
+	assert.NoError(t, eng.Run(context.Background(), state, "Type2_1"))
+	assert.ElementsMatch(t, []string{"Type1_2", "Type1_3"}, state.Stamps[0:2])
+	assert.Equal(t, "Type2_1", state.Stamps[2])
+	assert.Equal(t, []bool{true, true, false}, state.ConcurrentInfo)
+
+	state = new(State)
+	assert.NoError(t, eng.Run(context.Background(), state, "Type2_2"))
+	assert.ElementsMatch(t, []string{"Type1_1", "Type1_2", "Type1_4"}, state.Stamps[0:3])
+	assert.Equal(t, "Type2_2", state.Stamps[3])
+	assert.Equal(t, []bool{true, true}, state.ConcurrentInfo[:2])
+	assert.False(t, state.ConcurrentInfo[3])
+}
+
+func TestNormalNodeGraph4(t *testing.T) {
+	node := new(Node[*State])
+
+	eng := NewEngine[*State]("")
+	assert.NoError(t, AddNode(eng, NewNodeType3("Type1_1", nil)))
+	assert.NoError(t, AddNode(eng, NewNodeType3("Type1_2", nil)))
+	assert.NoError(t, AddNode(eng, NewNodeType3("Type1_3", nil)))
+	assert.NoError(t, AddNode(eng, NewNodeType3("Type1_4", []string{"Type1_1"})))
+	assert.NoError(t, AddNode(eng, NewNodeType3("Type1_5", []string{"Type1_6"})))
+	assert.NoError(t, AddNode(eng, NewNodeType3("Type1_6", nil)))
+
+	assert.NoError(t, AddNode(eng, NewNodeType4("Type2_1", []*Dependence[*State]{
+		node.StaticDependence("Type1_3"),
+		node.ConditionalDependence("Type1_5", func(ctx context.Context, s *State) bool { return true }, []string{"Type1_2"}),
+	})))
+	assert.NoError(t, AddNode(eng, NewNodeType4("Type2_2", []*Dependence[*State]{
+		node.ConditionalDependence("Type2_1", func(ctx context.Context, s *State) bool { return true }, []string{"Type1_2", "Type1_4"}),
+	})))
+
+	assert.NoError(t, eng.Prepare())
+	// fmt.Println(eng.Dot())
+
+	state := new(State)
+	assert.NoError(t, eng.Run(context.Background(), state, "Type1_1"))
+	assert.Equal(t, []string{"Type1_1"}, state.Stamps)
+	assert.Equal(t, []bool{false}, state.ConcurrentInfo)
+
+	state = new(State)
+	assert.NoError(t, eng.Run(context.Background(), state, "Type2_1"))
+	assert.ElementsMatch(t, []string{"Type1_2", "Type1_3"}, state.Stamps[0:2])
+	assert.Equal(t, "Type1_6", state.Stamps[2])
+	assert.Equal(t, "Type1_5", state.Stamps[3])
+	assert.Equal(t, "Type2_1", state.Stamps[4])
+	assert.Equal(t, []bool{true, true, false, false, false}, state.ConcurrentInfo)
+
+	state = new(State)
+	assert.NoError(t, eng.Run(context.Background(), state, "Type2_2"))
+	assert.ElementsMatch(t, []string{"Type1_1", "Type1_2", "Type1_4"}, state.Stamps[0:3])
+	assert.ElementsMatch(t, []string{"Type1_3", "Type1_6", "Type1_5"}, state.Stamps[3:6])
+	assert.Equal(t, "Type2_1", state.Stamps[6])
+	assert.Equal(t, "Type2_2", state.Stamps[7])
+	assert.Equal(t, []bool{true, true}, state.ConcurrentInfo[:2])
+	assert.Equal(t, []bool{true, true}, state.ConcurrentInfo[3:5])
+	assert.Equal(t, []bool{false, false}, state.ConcurrentInfo[6:])
+}
+
+func TestNormalNodeGraph5(t *testing.T) {
+	node := new(Node[*State])
+
+	eng := NewEngine[*State]("")
+	assert.NoError(t, AddNode(eng, NewNodeType3("Type1_1", nil)))
+	assert.NoError(t, AddNode(eng, NewNodeType3("Type1_2", nil)))
+	assert.NoError(t, AddNode(eng, NewNodeType3("Type1_3", nil)))
+	assert.NoError(t, AddNode(eng, NewNodeType3("Type1_4", []string{"Type1_1"})))
+	assert.NoError(t, AddNode(eng, NewNodeType3("Type1_5", []string{"Type1_6"})))
+	assert.NoError(t, AddNode(eng, NewNodeType3("Type1_6", nil)))
+
+	assert.NoError(t, AddNode(eng, NewNodeType4("Type2_1", []*Dependence[*State]{
+		node.StaticDependence("Type1_3"),
+		node.ConditionalDependence("Type1_5", func(ctx context.Context, s *State) bool { panic("") }, []string{"Type1_2"}),
+	})))
+	assert.NoError(t, AddNode(eng, NewNodeType4("Type2_2", []*Dependence[*State]{
+		node.ConditionalDependence("Type2_1", func(ctx context.Context, s *State) bool { panic("") }, []string{"Type1_2", "Type1_4"}),
+	})))
+
+	assert.NoError(t, eng.Prepare())
 	fmt.Println(eng.Dot())
+
+	state := new(State)
+	// assert.NoError(t, eng.Run(context.Background(), state, "Type1_1"))
+	// assert.Equal(t, []string{"Type1_1"}, state.Stamps)
+	// assert.Equal(t, []bool{false}, state.ConcurrentInfo)
+
+	// state = new(State)
+	assert.NoError(t, eng.Run(context.Background(), state, "Type2_1"))
+	assert.ElementsMatch(t, []string{"Type1_2", "Type1_3"}, state.Stamps[0:2])
+	assert.Equal(t, []bool{true, true}, state.ConcurrentInfo)
+
+	state = new(State)
+	assert.NoError(t, eng.Run(context.Background(), state, "Type2_2"))
+	assert.Equal(t, 3, len(state.Stamps))
+	assert.Equal(t, 3, len(state.ConcurrentInfo))
+	assert.ElementsMatch(t, []string{"Type1_1", "Type1_2", "Type1_4"}, state.Stamps[0:3])
+	assert.Equal(t, []bool{true, true}, state.ConcurrentInfo[:2])
 }
