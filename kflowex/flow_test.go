9589a182fb3ce4050fc8b24e0f1fccd27a463816
Kie <Kidsunbo@users.noreply.github.com>
2025-03-15 03:40:32 +0000

    [ADD] add kflowex

---
+++ kflowex/flow_test.go
@@ -0,0 +1,213 @@
+package kflowex_test
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"testing"
+
+	"github.com/Kidsunbo/kie_toolbox_go/kflowex"
+	"github.com/stretchr/testify/assert"
+)
+
+type State struct {
+	lock sync.Mutex
+	Step []string
+}
+
+func (s *State) AddStep(step string) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	s.Step = append(s.Step, step)
+}
+
+type Description[S any] struct {
+	Name       string
+	Dependence []string
+}
+
+func (d Description[S]) GetName() string {
+	return d.Name
+}
+
+func (d Description[S]) GetDependence() []kflowex.Dependence[*State] {
+	deps := make([]kflowex.Dependence[*State], 0, len(d.Dependence))
+	for _, dep := range d.Dependence {
+		deps = append(deps, kflowex.Dependence[*State]{
+			Name: dep,
+		})
+	}
+	return deps
+}
+
+type Node1 struct{}
+
+func NewNode1() *Node1 {
+	return &Node1{}
+}
+
+func (n *Node1) Description() Description[*State] {
+	return Description[*State]{
+		Name: "Node1",
+	}
+}
+
+func (n *Node1) Run(ctx context.Context, state *State) error {
+	state.AddStep("Node1")
+	return nil
+}
+
+type Node2 struct{}
+
+func NewNode2() *Node2 {
+	return &Node2{}
+}
+
+func (n *Node2) Description() Description[*State] {
+	return Description[*State]{
+		Name: "Node2",
+	}
+}
+
+func (n *Node2) Run(ctx context.Context, state *State) error {
+	state.AddStep("Node2")
+	return nil
+}
+
+type Node3 struct{}
+
+func NewNode3() *Node3 {
+	return &Node3{}
+}
+
+func (n *Node3) Description() Description[*State] {
+	return Description[*State]{
+		Name:       "Node3",
+		Dependence: []string{"Node1", "Node2"},
+	}
+}
+
+func (n *Node3) Run(ctx context.Context, state *State, plan *kflowex.Plan) error {
+	state.AddStep("Node3")
+	return nil
+}
+
+type AddMW[S kflowex.IState, D kflowex.IDescription[S]] struct {
+	name   string
+	values *[]string
+}
+
+func (a *AddMW[S, D]) Before(desc D) {
+	*a.values = append(*a.values, fmt.Sprintf("b_%v", a.name))
+}
+
+func (a *AddMW[S, D]) After(desc D, err error) {
+	*a.values = append(*a.values, fmt.Sprintf("a_%v", a.name))
+}
+
+type RunMW[S kflowex.IState, D kflowex.IDescription[S]] struct {
+	name string
+}
+
+func (a *RunMW[S, D]) Before(ctx context.Context, state S, desc D) {
+	any(state).(*State).AddStep(fmt.Sprintf("b_%v", a.name))
+}
+
+func (a *RunMW[S, D]) After(ctx context.Context, state S, desc D, err error) {
+	any(state).(*State).AddStep(fmt.Sprintf("a_%v", a.name))
+}
+
+func TestFlowAddNodeSuccess(t *testing.T) {
+	state := &State{
+		Step: []string{},
+	}
+	engine, err := kflowex.NewFlowBuilder("test", func(f *kflowex.Flow[*State, Description[*State]]) {
+		assert.NoError(t, kflowex.AddNode(f, NewNode1))
+		assert.NoError(t, kflowex.AddNode(f, NewNode2))
+	}).Build()
+	assert.NoError(t, err)
+	assert.NoError(t, engine.Run(context.Background(), state, "Node1", "Node2"))
+	assert.Equal(t, []string{"Node1", "Node2"}, state.Step)
+}
+
+func TestFlowAddNodeFail(t *testing.T) {
+	state := &State{
+		Step: []string{},
+	}
+	engine, err := kflowex.NewFlowBuilder("test", func(f *kflowex.Flow[*State, Description[*State]]) {
+		assert.NoError(t, kflowex.AddNode(f, NewNode1))
+		assert.NoError(t, kflowex.AddNode(f, NewNode2))
+		assert.Error(t, kflowex.AddNode(f, NewNode1))
+	}).Build()
+	assert.NoError(t, err)
+	assert.NoError(t, engine.Run(context.Background(), state, "Node1", "Node2"))
+	assert.Error(t, engine.Run(context.Background(), state, "Node1", "Node3"))
+	assert.Equal(t, []string{"Node1", "Node2"}, state.Step)
+}
+
+func TestFlowNodeDescription(t *testing.T) {
+	state := &State{
+		Step: []string{},
+	}
+	engine, err := kflowex.NewFlowBuilder("test", func(f *kflowex.Flow[*State, Description[*State]]) {
+		assert.NoError(t, kflowex.AddNode(f, NewNode1))
+		assert.NoError(t, kflowex.AddNode(f, NewNode2))
+		assert.NoError(t, kflowex.AddNode(f, NewNode3))
+	}).Build()
+	assert.NoError(t, err)
+	assert.NoError(t, engine.Run(context.Background(), state, "Node1", "Node2"))
+	assert.Equal(t, []string{"Node1", "Node2"}, state.Step)
+
+	state = &State{
+		Step: []string{},
+	}
+	assert.NoError(t, engine.Run(context.Background(), state, "Node3"))
+	assert.Equal(t, 3, len(state.Step))
+	assert.ElementsMatch(t, []string{"Node1", "Node2"}, state.Step[:2])
+	assert.Equal(t, "Node3", state.Step[2])
+}
+
+func TestFlowNodeMiddleware(t *testing.T) {
+	state := &State{
+		Step: []string{},
+	}
+
+	step := make([]string, 0)
+	addMW1 := &AddMW[*State, Description[*State]]{
+		name:   "addMW1",
+		values: &step,
+	}
+	addMW2 := &AddMW[*State, Description[*State]]{
+		name:   "addMW2",
+		values: &step,
+	}
+
+	_, err := kflowex.NewFlowBuilder("test", func(f *kflowex.Flow[*State, Description[*State]]) {
+		assert.NoError(t, kflowex.AddNode(f, NewNode1))
+		assert.NoError(t, kflowex.AddNode(f, NewNode2))
+		assert.NoError(t, kflowex.AddNode(f, NewNode3))
+	}).WithAddMiddleware(addMW1, addMW2).Build()
+	assert.NoError(t, err)
+	assert.Equal(t, []string{"b_addMW1", "b_addMW2", "a_addMW2", "a_addMW1", "b_addMW1", "b_addMW2", "a_addMW2", "a_addMW1", "b_addMW1", "b_addMW2", "a_addMW2", "a_addMW1"}, step)
+
+	runMW1 := &RunMW[*State, Description[*State]]{
+		name: "runMW1",
+	}
+	runMW2 := &RunMW[*State, Description[*State]]{
+		name: "runMW2",
+	}
+	engine, err := kflowex.NewFlowBuilder("test", func(f *kflowex.Flow[*State, Description[*State]]) {
+		assert.NoError(t, kflowex.AddNode(f, NewNode1))
+		assert.NoError(t, kflowex.AddNode(f, NewNode2))
+		assert.NoError(t, kflowex.AddNode(f, NewNode3))
+	}).WithRunMiddleware(runMW1, runMW2).Build()
+	assert.NoError(t, err)
+
+	state = &State{
+		Step: []string{},
+	}
+	assert.NoError(t, engine.Run(context.Background(), state, "Node3"))
+	assert.Equal(t, 15, len(state.Step))
+	assert.ElementsMatch(t, []string{"b_runMW1", "b_runMW2", "Node1", "a_runMW2", "a_runMW1", "b_runMW1", "b_runMW2", "Node2", "a_runMW2", "a_runMW1"}, state.Step[:10])
+	assert.Equal(t, []string{"b_runMW1", "b_runMW2", "Node3", "a_runMW2", "a_runMW1"}, state.Step[10:15])
+}
---
+++ kflowex/wrapper.go
@@ -0,0 +1,64 @@
+package kflowex
+
+import (
+	"context"
+	"errors"
+
+	"github.com/Kidsunbo/kie_toolbox_go/kflow"
+)
+
+type nodeWrapper[S IState, D IDescription[S], T INode[S, D]] struct {
+	kflow.Node[S]
+	mw          []IRunMiddleware[S, D]
+	node        T
+	constructor func() T
+}
+
+func (n *nodeWrapper[S, D, T]) Name() string {
+	return n.node.Description().GetName()
+}
+
+func (n *nodeWrapper[S, D, T]) Dependence() []*kflow.Dependence[S] {
+	description := n.node.Description()
+	dependences := make([]*kflow.Dependence[S], 0, len(description.GetDependence()))
+	for _, dependence := range description.GetDependence() {
+		if dependence.Function == nil {
+			dependences = append(dependences, n.StaticDependence(dependence.Name))
+		} else {
+			dependences = append(dependences, n.ConditionalDependence(dependence.Name, kflow.Condition[S](dependence.Function), dependence.Dependences))
+		}
+	}
+	return dependences
+}
+
+func (n *nodeWrapper[S, D, T]) Run(ctx context.Context, state S, plan *Plan) error {
+	v := n.constructor()
+	desc := v.Description()
+
+	for i := 0; i < len(n.mw); i++ {
+		n.mw[i].Before(ctx, state, desc)
+	}
+
+	var err error
+	if value, ok := any(v).(kflow.IBasicNode[S]); ok {
+		err = value.Run(ctx, state)
+	} else if value, ok := any(v).(kflow.IFlowNode[S]); ok {
+		err = value.Run(ctx, state, plan)
+	} else {
+		return errors.New("not supported")
+	}
+
+	for i := len(n.mw) - 1; i >= 0; i-- {
+		n.mw[i].After(ctx, state, desc, err)
+	}
+	return err
+}
+
+func wrap[S any, D IDescription[S], T INode[S, D]](constructor func() T, mw []IRunMiddleware[S, D]) *nodeWrapper[S, D, T] {
+	node := constructor()
+	return &nodeWrapper[S, D, T]{
+		mw:          mw,
+		node:        node,
+		constructor: constructor,
+	}
+}
