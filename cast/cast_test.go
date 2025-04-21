1553d317514d3bda4e03e1849a9bc7574f09312e
Kie <Kidsunbo@users.noreply.github.com>
2025-04-21 06:48:09 +0000

    [UPDATE] update the version requirement to go 1.21

--- cast/cast_test.go
+++ cast/cast_test.go
@@ -1,325 +1,326 @@
-package cast
+package cast_test
 
 import (
 	"testing"
 
+	"github.com/Kidsunbo/kie_toolbox_go/cast"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestToBool(t *testing.T) {
 	s := "t"
-	v, err := To[bool](s)
+	v, err := cast.To[bool](s)
 	assert.Equal(t, v, true)
 	assert.Equal(t, nil, err)
 
 	s = "tr"
-	v, err = To[bool](s)
+	v, err = cast.To[bool](s)
 	assert.Equal(t, v, false)
 	assert.NotEqual(t, nil, err)
 
 	s = "true"
-	v, err = To[bool](s)
+	v, err = cast.To[bool](s)
 	assert.Equal(t, v, true)
 	assert.Equal(t, nil, err)
 
 	s = "f"
-	v, err = To[bool](s)
+	v, err = cast.To[bool](s)
 	assert.Equal(t, v, false)
 	assert.Equal(t, nil, err)
 
 	s = "fa"
-	v, err = To[bool](s)
+	v, err = cast.To[bool](s)
 	assert.Equal(t, v, false)
 	assert.NotEqual(t, nil, err)
 
 	s = "false"
-	v, err = To[bool](s)
+	v, err = cast.To[bool](s)
 	assert.Equal(t, v, false)
 	assert.Equal(t, nil, err)
 
 	s = "0"
-	v, err = To[bool](s)
+	v, err = cast.To[bool](s)
 	assert.Equal(t, v, false)
 	assert.Equal(t, nil, err)
 
 	s = "-1"
-	v, err = To[bool](s)
+	v, err = cast.To[bool](s)
 	assert.Equal(t, v, false)
 	assert.NotEqual(t, nil, err)
 
 	s = "-"
-	v, err = To[bool](s)
+	v, err = cast.To[bool](s)
 	assert.Equal(t, v, false)
 	assert.NotEqual(t, nil, err)
 
 	s = "123"
-	v, err = To[bool](s)
+	v, err = cast.To[bool](s)
 	assert.Equal(t, v, false)
 	assert.NotEqual(t, nil, err)
 
 	s = "1"
-	v, err = To[bool](s)
+	v, err = cast.To[bool](s)
 	assert.Equal(t, v, true)
 	assert.Equal(t, nil, err)
 
 	s = "2"
-	v, err = To[bool](s)
+	v, err = cast.To[bool](s)
 	assert.Equal(t, v, false)
 	assert.NotEqual(t, nil, err)
 }
 
 func TestToString(t *testing.T) {
 	s := "t"
-	v, err := To[string](s)
+	v, err := cast.To[string](s)
 	assert.Equal(t, v, "t")
 	assert.Equal(t, nil, err)
 
 	s = "tr"
-	v, err = To[string](s)
+	v, err = cast.To[string](s)
 	assert.Equal(t, v, "tr")
 	assert.Equal(t, nil, err)
 }
 
 func TestToInt(t *testing.T) {
 	s := "t"
-	v, err := To[int](s)
+	v, err := cast.To[int](s)
 	assert.Equal(t, v, 0)
 	assert.NotEqual(t, nil, err)
 
 	s = "123"
-	v, err = To[int](s)
+	v, err = cast.To[int](s)
 	assert.Equal(t, v, 123)
 	assert.Equal(t, nil, err)
 }
 
 func TestToInt8(t *testing.T) {
 	s := "t"
-	v, err := To[int8](s)
+	v, err := cast.To[int8](s)
 	assert.Equal(t, v, int8(0))
 	assert.NotEqual(t, nil, err)
 
 	s = "123"
-	v, err = To[int8](s)
+	v, err = cast.To[int8](s)
 	assert.Equal(t, v, int8(123))
 	assert.Equal(t, nil, err)
 
 	s = "t"
-	v, err = ToInteger[int8](s)
+	v, err = cast.ToInteger[int8](s)
 	assert.Equal(t, v, int8(0))
 	assert.NotEqual(t, nil, err)
 
 	s = "123"
-	v, err = ToInteger[int8](s)
+	v, err = cast.ToInteger[int8](s)
 	assert.Equal(t, v, int8(123))
 	assert.Equal(t, nil, err)
 }
 
 func TestToInt16(t *testing.T) {
 	s := "t"
-	v, err := To[int16](s)
+	v, err := cast.To[int16](s)
 	assert.Equal(t, v, int16(0))
 	assert.NotEqual(t, nil, err)
 
 	s = "123"
-	v, err = To[int16](s)
+	v, err = cast.To[int16](s)
 	assert.Equal(t, v, int16(123))
 	assert.Equal(t, nil, err)
 
 	s = "t"
-	v, err = ToInteger[int16](s)
+	v, err = cast.ToInteger[int16](s)
 	assert.Equal(t, v, int16(0))
 	assert.NotEqual(t, nil, err)
 
 	s = "123"
-	v, err = ToInteger[int16](s)
+	v, err = cast.ToInteger[int16](s)
 	assert.Equal(t, v, int16(123))
 	assert.Equal(t, nil, err)
 }
 
 func TestToInt32(t *testing.T) {
 	s := "t"
-	v, err := To[int32](s)
+	v, err := cast.To[int32](s)
 	assert.Equal(t, v, int32(0))
 	assert.NotEqual(t, nil, err)
 
 	s = "123"
-	v, err = To[int32](s)
+	v, err = cast.To[int32](s)
 	assert.Equal(t, v, int32(123))
 	assert.Equal(t, nil, err)
 
 	s = "t"
-	v, err = ToInteger[int32](s)
+	v, err = cast.ToInteger[int32](s)
 	assert.Equal(t, v, int32(0))
 	assert.NotEqual(t, nil, err)
 
 	s = "123"
-	v, err = ToInteger[int32](s)
+	v, err = cast.ToInteger[int32](s)
 	assert.Equal(t, v, int32(123))
 	assert.Equal(t, nil, err)
 }
 
 func TestToInt64(t *testing.T) {
 	s := "t"
-	v, err := To[int64](s)
+	v, err := cast.To[int64](s)
 	assert.Equal(t, v, int64(0))
 	assert.NotEqual(t, nil, err)
 
 	s = "123"
-	v, err = To[int64](s)
+	v, err = cast.To[int64](s)
 	assert.Equal(t, v, int64(123))
 	assert.Equal(t, nil, err)
 
 	s = "t"
-	v, err = ToInteger[int64](s)
+	v, err = cast.ToInteger[int64](s)
 	assert.Equal(t, v, int64(0))
 	assert.NotEqual(t, nil, err)
 
 	s = "123"
-	v, err = ToInteger[int64](s)
+	v, err = cast.ToInteger[int64](s)
 	assert.Equal(t, v, int64(123))
 	assert.Equal(t, nil, err)
 }
 
 func TestToUint(t *testing.T) {
 	s := "t"
-	v, err := To[uint](s)
+	v, err := cast.To[uint](s)
 	assert.Equal(t, v, uint(0))
 	assert.NotEqual(t, nil, err)
 
 	s = "123"
-	v, err = To[uint](s)
+	v, err = cast.To[uint](s)
 	assert.Equal(t, v, uint(123))
 	assert.Equal(t, nil, err)
 
 	s = "t"
-	v, err = ToInteger[uint](s)
+	v, err = cast.ToInteger[uint](s)
 	assert.Equal(t, v, uint(0))
 	assert.NotEqual(t, nil, err)
 
 	s = "123"
-	v, err = ToInteger[uint](s)
+	v, err = cast.ToInteger[uint](s)
 	assert.Equal(t, v, uint(123))
 	assert.Equal(t, nil, err)
 }
 
 func TestToUint8(t *testing.T) {
 	s := "t"
-	v, err := To[uint8](s)
+	v, err := cast.To[uint8](s)
 	assert.Equal(t, v, uint8(0))
 	assert.NotEqual(t, nil, err)
 
 	s = "123"
-	v, err = To[uint8](s)
+	v, err = cast.To[uint8](s)
 	assert.Equal(t, v, uint8(123))
 	assert.Equal(t, nil, err)
 
 	s = "t"
-	v, err = ToInteger[uint8](s)
+	v, err = cast.ToInteger[uint8](s)
 	assert.Equal(t, v, uint8(0))
 	assert.NotEqual(t, nil, err)
 
 	s = "123"
-	v, err = ToInteger[uint8](s)
+	v, err = cast.ToInteger[uint8](s)
 	assert.Equal(t, v, uint8(123))
 	assert.Equal(t, nil, err)
 }
 
 func TestToUint16(t *testing.T) {
 	s := "t"
-	v, err := To[uint16](s)
+	v, err := cast.To[uint16](s)
 	assert.Equal(t, v, uint16(0))
 	assert.NotEqual(t, nil, err)
 
 	s = "123"
-	v, err = To[uint16](s)
+	v, err = cast.To[uint16](s)
 	assert.Equal(t, v, uint16(123))
 	assert.Equal(t, nil, err)
 
 	s = "t"
-	v, err = ToInteger[uint16](s)
+	v, err = cast.ToInteger[uint16](s)
 	assert.Equal(t, v, uint16(0))
 	assert.NotEqual(t, nil, err)
 
 	s = "123"
-	v, err = ToInteger[uint16](s)
+	v, err = cast.ToInteger[uint16](s)
 	assert.Equal(t, v, uint16(123))
 	assert.Equal(t, nil, err)
 }
 
 func TestToUint32(t *testing.T) {
 	s := "t"
-	v, err := To[uint32](s)
+	v, err := cast.To[uint32](s)
 	assert.Equal(t, v, uint32(0))
 	assert.NotEqual(t, nil, err)
 
 	s = "123"
-	v, err = To[uint32](s)
+	v, err = cast.To[uint32](s)
 	assert.Equal(t, v, uint32(123))
 	assert.Equal(t, nil, err)
 
 	s = "t"
-	v, err = ToInteger[uint32](s)
+	v, err = cast.ToInteger[uint32](s)
 	assert.Equal(t, v, uint32(0))
 	assert.NotEqual(t, nil, err)
 
 	s = "123"
-	v, err = ToInteger[uint32](s)
+	v, err = cast.ToInteger[uint32](s)
 	assert.Equal(t, v, uint32(123))
 	assert.Equal(t, nil, err)
 }
 
 func TestToUint64(t *testing.T) {
 	s := "t"
-	v, err := To[uint64](s)
+	v, err := cast.To[uint64](s)
 	assert.Equal(t, v, uint64(0))
 	assert.NotEqual(t, nil, err)
 
 	s = "123"
-	v, err = To[uint64](s)
+	v, err = cast.To[uint64](s)
 	assert.Equal(t, v, uint64(123))
 	assert.Equal(t, nil, err)
 
 	s = "t"
-	v, err = ToInteger[uint64](s)
+	v, err = cast.ToInteger[uint64](s)
 	assert.Equal(t, v, uint64(0))
 	assert.NotEqual(t, nil, err)
 
 	s = "123"
-	v, err = ToInteger[uint64](s)
+	v, err = cast.ToInteger[uint64](s)
 	assert.Equal(t, v, uint64(123))
 	assert.Equal(t, nil, err)
 }
 
 func TestToFloat32(t *testing.T) {
 	s := "t"
-	v, err := To[float32](s)
+	v, err := cast.To[float32](s)
 	assert.Equal(t, v, float32(0))
 	assert.NotEqual(t, nil, err)
 
 	s = "123"
-	v, err = To[float32](s)
+	v, err = cast.To[float32](s)
 	assert.Equal(t, v, float32(123))
 	assert.Equal(t, nil, err)
 
 	s = "123.456"
-	v, err = To[float32](s)
+	v, err = cast.To[float32](s)
 	assert.Equal(t, v, float32(123.456))
 	assert.Equal(t, nil, err)
 }
 
 func TestToFloat64(t *testing.T) {
 	s := "t"
-	v, err := To[float64](s)
+	v, err := cast.To[float64](s)
 	assert.Equal(t, v, float64(0))
 	assert.NotEqual(t, nil, err)
 
 	s = "123"
-	v, err = To[float64](s)
+	v, err = cast.To[float64](s)
 	assert.Equal(t, v, float64(123))
 	assert.Equal(t, nil, err)
 
 	s = "123.456"
-	v, err = To[float64](s)
+	v, err = cast.To[float64](s)
 	assert.Equal(t, v, 123.456)
 	assert.Equal(t, nil, err)
 }
@@ -327,20 +328,20 @@ func TestToFloat64(t *testing.T) {
 func TestToDefault(t *testing.T) {
 	right := "123"
 	wrong := "abc"
-	assert.Equal(t, int8(123), ToWithDefault[int8](right, 0))
-	assert.Equal(t, int8(0), ToWithDefault[int8](wrong, 0))
+	assert.Equal(t, int8(123), cast.ToWithDefault[int8](right, 0))
+	assert.Equal(t, int8(0), cast.ToWithDefault[int8](wrong, 0))
 
-	assert.Equal(t, int16(123), ToWithDefault[int16](right, 0))
-	assert.Equal(t, int16(0), ToWithDefault[int16](wrong, 0))
+	assert.Equal(t, int16(123), cast.ToWithDefault[int16](right, 0))
+	assert.Equal(t, int16(0), cast.ToWithDefault[int16](wrong, 0))
 
-	assert.Equal(t, uint8(123), ToWithDefault[uint8](right, 0))
-	assert.Equal(t, uint8(0), ToWithDefault[uint8](wrong, 0))
+	assert.Equal(t, uint8(123), cast.ToWithDefault[uint8](right, 0))
+	assert.Equal(t, uint8(0), cast.ToWithDefault[uint8](wrong, 0))
 
-	assert.Equal(t, uint16(123), ToWithDefault[uint16](right, 0))
-	assert.Equal(t, uint16(0), ToWithDefault[uint16](wrong, 0))
+	assert.Equal(t, uint16(123), cast.ToWithDefault[uint16](right, 0))
+	assert.Equal(t, uint16(0), cast.ToWithDefault[uint16](wrong, 0))
 
-	assert.Equal(t, uint16(123), ToIntegerWithDefault[uint16](right, 0))
-	assert.Equal(t, uint16(0), ToIntegerWithDefault[uint16](wrong, 0))
+	assert.Equal(t, uint16(123), cast.ToIntegerWithDefault[uint16](right, 0))
+	assert.Equal(t, uint16(0), cast.ToIntegerWithDefault[uint16](wrong, 0))
 }
 
 func TestToUnderlyingType(t *testing.T) {
@@ -351,27 +352,27 @@ func TestToUnderlyingType(t *testing.T) {
 	type UnderlyingBool bool
 
 	s := "123"
-	v, err := To[UnderlyingInt](s)
+	v, err := cast.To[UnderlyingInt](s)
 	assert.Equal(t, UnderlyingInt(123), v)
 	assert.Equal(t, nil, err)
 
 	s = "t"
-	vb, err := To[UnderlyingBool](s)
+	vb, err := cast.To[UnderlyingBool](s)
 	assert.Equal(t, UnderlyingBool(true), vb)
 	assert.Equal(t, nil, err)
 
 	s = "hello"
-	vs, err := To[UnderlyingString](s)
+	vs, err := cast.To[UnderlyingString](s)
 	assert.Equal(t, UnderlyingString("hello"), vs)
 	assert.Equal(t, nil, err)
 
 	s = "123.456"
-	vf32, err := To[UnderlyingFloat32](s)
+	vf32, err := cast.To[UnderlyingFloat32](s)
 	assert.Equal(t, UnderlyingFloat32(123.456), vf32)
 	assert.Equal(t, nil, err)
 
 	s = "123.456"
-	vf64, err := To[UnderlyingFloat64](s)
+	vf64, err := cast.To[UnderlyingFloat64](s)
 	assert.Equal(t, UnderlyingFloat64(123.456), vf64)
 	assert.Equal(t, nil, err)
 }
@@ -381,21 +382,21 @@ func TestJSONTo(t *testing.T) {
 		Hello string `json:"hello"`
 		World int64  `json:"world"`
 	}
-	v, err := JSONTo[*s](`{"world": 1234567, "hello":"hey"}`)
+	v, err := cast.JSONTo[*s](`{"world": 1234567, "hello":"hey"}`)
 	assert.Equal(t, nil, err)
 	assert.Equal(t, "hey", v.Hello)
 	assert.Equal(t, int64(1234567), v.World)
 
-	v, err = JSONTo[*s](`{"cat":"wolf", "dog":"meow"}`)
+	v, err = cast.JSONTo[*s](`{"cat":"wolf", "dog":"meow"}`)
 	assert.Equal(t, nil, err)
 	assert.Equal(t, "", v.Hello)
 	assert.Equal(t, int64(0), v.World)
 
-	v, err = JSONTo[*s](`{"cat":"wolf", "dog":"meow"`)
+	v, err = cast.JSONTo[*s](`{"cat":"wolf", "dog":"meow"`)
 	assert.NotEqual(t, nil, err)
 	assert.True(t, v == nil)
 
-	a, err := JSONTo[[]string](`["hello", "world"]`)
+	a, err := cast.JSONTo[[]string](`["hello", "world"]`)
 	assert.Equal(t, nil, err)
 	assert.Equal(t, 2, len(a))
 	assert.Equal(t, "hello", a[0])
