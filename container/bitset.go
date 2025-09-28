5b514ab84c499f519e26e9c5984bed46e7175865
Kie <Kidsunbo@users.noreply.github.com>
2025-09-28 06:55:44 +0000

    [ADD] add bit set

---
+++ container/bitset.go
@@ -0,0 +1,90 @@
+package container
+
+import (
+	"sync/atomic"
+)
+
+type AtomicBitSet struct {
+	bits []atomic.Uint64
+}
+
+func NewAtomicBitSet(bits int) *AtomicBitSet {
+	return &AtomicBitSet{
+		bits: make([]atomic.Uint64, (bits+63)>>6),
+	}
+}
+
+func (a *AtomicBitSet) Set(bit int) {
+	if bit < 0 || bit >= len(a.bits)<<6 {
+		panic("bit index out of range")
+	}
+	a.bits[bit>>6].Or(1 << (bit & 63))
+}
+
+func (a *AtomicBitSet) Clear(bit int) {
+	if bit < 0 || bit >= len(a.bits)<<6 {
+		panic("bit index out of range")
+	}
+	a.bits[bit>>6].And(^uint64(1 << (bit & 63)))
+}
+
+func (a *AtomicBitSet) Get(bit int) bool {
+	if bit < 0 || bit >= len(a.bits)<<6 {
+		panic("bit index out of range")
+	}
+	return a.bits[bit>>6].Load()&(1<<(bit&63)) != 0
+}
+
+func (a *AtomicBitSet) Any(start int, end int) bool {
+	if start < 0 || end < 0 || start >= end {
+		panic("invalid range")
+	}
+	startWord, startBit := start>>6, start&63
+	endWord, endBit := (end-1)>>6, (end-1)&63
+	if startWord >= len(a.bits) || endWord >= len(a.bits) {
+		panic("bit index out of range")
+	}
+
+	if startWord == endWord {
+		mask := ((uint64(1) << (endBit + 1)) - 1) & ^((uint64(1) << startBit) - 1)
+		return a.bits[startWord].Load()&mask != 0
+	}
+
+	if a.bits[startWord].Load()&^((uint64(1)<<startBit)-1) != 0 {
+		return true
+	}
+	for i := startWord + 1; i < endWord; i++ {
+		if a.bits[i].Load() != 0 {
+			return true
+		}
+	}
+	return a.bits[endWord].Load()&((uint64(1)<<(endBit+1))-1) != 0
+}
+
+func (a *AtomicBitSet) All(start int, end int) bool {
+	if start < 0 || end < 0 || start >= end {
+		panic("invalid range")
+	}
+	startWord, startBit := start>>6, start&63
+	endWord, endBit := (end-1)>>6, (end-1)&63
+	if startWord >= len(a.bits) || endWord >= len(a.bits) {
+		panic("bit index out of range")
+	}
+
+	if startWord == endWord {
+		mask := ((uint64(1) << (endBit + 1)) - 1) & ^((uint64(1) << startBit) - 1)
+		return a.bits[startWord].Load()&mask == mask
+	}
+
+	mask := ^((uint64(1) << startBit) - 1)
+	if a.bits[startWord].Load()&mask != mask {
+		return false
+	}
+	for i := startWord + 1; i < endWord; i++ {
+		if a.bits[i].Load() != ^uint64(0) {
+			return false
+		}
+	}
+	mask = (uint64(1) << (endBit + 1)) - 1
+	return a.bits[endWord].Load()&mask == mask
+}
---
+++ container/bitset_test.go
@@ -0,0 +1,89 @@
+package container
+import (
+	"testing"
+)
+
+func TestAtomicBitSet_SetGet(t *testing.T) {
+	bs := NewAtomicBitSet(130)
+	bs.Set(0)
+	bs.Set(64)
+	bs.Set(129)
+
+	if !bs.Get(0) {
+		t.Errorf("bit 0 should be set")
+	}
+	if !bs.Get(64) {
+		t.Errorf("bit 64 should be set")
+	}
+	if !bs.Get(129) {
+		t.Errorf("bit 129 should be set")
+	}
+	if bs.Get(1) {
+		t.Errorf("bit 1 should not be set")
+	}
+	if bs.Get(128) {
+		t.Errorf("bit 128 should not be set")
+	}
+}
+
+func TestAtomicBitSet_Clear(t *testing.T) {
+	bs := NewAtomicBitSet(70)
+	bs.Set(10)
+	bs.Set(69)
+	bs.Clear(10)
+	bs.Clear(69)
+
+	if bs.Get(10) {
+		t.Errorf("bit 10 should be cleared")
+	}
+	if bs.Get(69) {
+		t.Errorf("bit 69 should be cleared")
+	}
+}
+
+func TestAtomicBitSet_Any(t *testing.T) {
+	bs := NewAtomicBitSet(100)
+	bs.Set(10)
+	bs.Set(50)
+	bs.Set(99)
+
+	if !bs.Any(10, 11) {
+		t.Errorf("Any should be true for set bit")
+	}
+	if bs.Any(11, 20) {
+		t.Errorf("Any should be false for unset range")
+	}
+	if !bs.Any(0, 51) {
+		t.Errorf("Any should be true for range including set bits")
+	}
+	if !bs.Any(90, 100) {
+		t.Errorf("Any should be true for range including bit 99")
+	}
+}
+
+func TestAtomicBitSet_All(t *testing.T) {
+	bs := NewAtomicBitSet(10)
+	for i := 0; i < 10; i++ {
+		bs.Set(i)
+	}
+	if !bs.All(0, 10) {
+		t.Errorf("All should be true when all bits are set")
+	}
+	bs.Clear(5)
+	if bs.All(0, 10) {
+		t.Errorf("All should be false when a bit is cleared")
+	}
+	if !bs.All(0, 5) {
+		t.Errorf("All should be true for range where all bits are set")
+	}
+}
+
+func TestAtomicBitSet_Panics(t *testing.T) {
+	bs := NewAtomicBitSet(10)
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("Expected panic for out of range bit")
+		}
+	}()
+	bs.Set(100)
+}
